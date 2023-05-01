139d60930e9cb04e1d53d2e248364e851624b965
HsingKuangLai <R11722016@ntu.edu.tw>
2023-05-01 23:25:52 +0800

    Add_python

---
+++ cut.py
@@ -0,0 +1,30 @@
+# Read in the Excel file as a dataframe
+
+import pandas as pd
+import numpy as np
+import openpyxl
+import os
+from openpyxl import load_workbook
+from pdfrw import PdfReader, PdfWriter
+
+
+excel_name="C:\Users\star8\Desktop\OP-copy\OP\2881\2881_110_etr.xlsm"
+filename="C:\Users\star8\Desktop\OP-copy\IP\2881_110.pdf"
+
+df = pd.read_excel(excel_name, engine='openpyxl')
+df
+
+# Loop through each row in the DataFrame
+for i in range(df.shape[0]):
+    # Split the pages string into individual page numbers
+    pages = list(map(int, df.loc[i, 'pages'].split(',')))
+    
+    # Generate the corresponding PDF file
+    output_file = os.path.join("C:/Users/pojid/01/output", str(i+1) + '.pdf')
+    writer = PdfWriter()
+    for page in pages:
+        writer.addpage(PdfReader(filename).pages[page-1])
+    writer.write(output_file)
+    
+    # Update the pdf_path column in the DataFrame
+    df.loc[i, 'pdf_path'] = output_file
