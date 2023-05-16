a46bf8b3df8bb5288a6e57212dd485027fbd4952
HsingKuangLai <R11722016@ntu.edu.tw>
2023-05-16 13:38:50 +0800

    串GD

--- cut.py
+++ cut.py
@@ -1,30 +1,48 @@
-# Read in the Excel file as a dataframe
+def cut(company_code, year_code):
+ # Read in the Excel file as a dataframe
 
-import pandas as pd
-import numpy as np
-import openpyxl
-import os
-from openpyxl import load_workbook
-from pdfrw import PdfWriter,PdfReader
+ import pandas as pd
+ import numpy as np
+ import openpyxl
+ import os
+ import re
+ from openpyxl import load_workbook
+ from pdfrw import PdfReader, PdfWriter
+
+ output_excel = 'outputfiles/' + company_code + '_' + year_code + '_etr.xlsx'
+ if not os.path.exists(output_excel):
+   output_excel = 'outputfiles/' + company_code + '_' + year_code + '_etr.xlsm'
+ 
+ filename='inputfiles/'+company_code+'_'+year_code+'.pdf'
+
+ df = pd.read_excel(output_excel, engine='openpyxl')
+
+ for i in range(df.shape[0]):
+     # Check if the pages column contains a string
+     #if isinstance(df.loc[i, 'pages'], str):
+         # Extract digits from the pages string
+   digits = re.findall(r'\d+', str(df.iloc[i, 1]))
+   pages = list(map(int, digits))
+
+         # Generate the corresponding PDF file
+   writer = PdfWriter()
+   output_file = os.path.join('outputfiles/'+company_code+'_'+year_code+'_'+str(df.iloc[i, 0]) + '.pdf')
+        
+   for page in pages:
+      writer.compress = True
+      writer.addpage(PdfReader(filename).pages[page-1])
+         # 壓縮 PDF
+      writer.compress = True   
+         
+   with open(output_file, 'wb') as f:
+       writer.compress = True
+       writer.write(f)
+       writer.compress = True
+         
+        #  # Update the pdf_path column in the DataFrame
+        #  df.loc[i, 'pdf_path'] = company_code+'_'+year_code+'_'+str(df.iloc[i, 0]) + '.pdf'
+ output_excel = 'outputfiles/' + company_code + '_' + year_code + '_etr.xlsx'
+ df.to_excel(output_excel,index=False,sheet_name=company_code)
 
 
-excel_name="C://Users//star8//Desktop//OP-copy//OP//2881//2881_110_etr.xlsx"
-filename="C://Users//star8//Desktop//OP-copy//IP//2881_110.pdf"
 
-df = pd.read_excel(excel_name, engine='openpyxl')
-df
-
-# Loop through each row in the DataFrame
-for i in range(df.shape[0]):
-    # Split the pages string into individual page numbers
-    pages = list(map(int, df.loc[i, 'pages'].split(',')))
-    
-    # Generate the corresponding PDF file
-    output_file = os.path.join("outputfiles/", str(i+1) + '.pdf')
-    writer = PdfWriter()
-    for page in pages:
-        writer.addpage(PdfReader(filename).pages[page-1])
-    writer.write(output_file)
-    
-    # Update the pdf_path column in the DataFrame
-    df.loc[i, 'pdf_path'] = output_file
